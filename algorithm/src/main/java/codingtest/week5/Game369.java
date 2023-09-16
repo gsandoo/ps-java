d3bc3c11cce928eb970679e78b5c6e470b07b18c
gsandoo <rogersddt@naver.com>
2023-09-17 00:39:17 +0900

    2023-09-17

--- algorithm/src/main/java/codingtest/week5/Game369.java
+++ algorithm/src/main/java/codingtest/week5/Game369.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class Game369 {
     public String solution(int a){
-        String answer = "clap";
+        String answer = "";
         if (a > 0 && a < 999) {
-            while (a > 3){
-                if ((a%10) % 3 ==0){
+            while (a >= 3){
+                if ((a%10) % 3 ==0 ){
                     answer += "clap";
                     a = a/10;
                 }else{
                     a = a/10;
                 }
-            }return answer;
+            }if (answer != "") return answer;
+            else return "clap";
         }else{
             return "0보다 작거나 999보다 큰 수를 입력하였습니다.";
         }
