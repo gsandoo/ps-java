1f0e2dc1aff78ed505624eab192bc81918090802
sakang <sakang@businessinsight.co.kr>
2024-05-10 10:43:27 +0900

    swea day 4

--- algorithm/src/main/java/com/example/algorithm/ssafy/Test3.java
+++ algorithm/src/main/java/com/example/algorithm/ssafy/Test3.java
@@ -1,9 +1,6 @@
 package com.example.algorithm.ssafy;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Test3 {
@@ -22,37 +19,36 @@ public class Test3 {
             words = num.split(" ");
             list.add(words);
         }
-        ArrayList<String> resultList = new ArrayList<>();
 /**
  *
  2
- 3 15 20 60 75 80 100
- 4 90 90 120 120 120 150 160 200
+ 3 15 20 60 75 80 100 -> 15 60 75
+ 4 90 90 120 120 120 150 160 200 -> 90 90 120 150
  */
         int num = 0;
-
+        ArrayList<String> resultList = new ArrayList<>();
+        List<String> answerList = new ArrayList<>();
         for (String[] x : list){
             num++;
-            resultList.add("#"+num+" ");
             List<String> temp = new ArrayList<>(Arrays.asList(x));
-            for (int i = 0 ; i < temp.size() ; i++){
-
-                int Q = Integer.parseInt(temp.get(i)) * 4 / 3 ;
-
-                for (int h = 0 ; h < temp.size() ; h++){
-                    int G = Integer.parseInt(temp.get(h));
-                    if (Q == G){
-                        resultList.add( temp.get(i));
-                        temp.remove(i);
-                        i--;
-                        break;
-                    }
+            while (temp.size() >= 2){
+                int val  = Integer.parseInt(temp.get(temp.size()-1));
+                int dis = val/4*3;
+                if (temp.contains(String.valueOf(dis))){
+                    resultList.add(String.valueOf(dis));
+                    temp.remove(String.valueOf(val));
+                    temp.remove(String.valueOf(dis));
                 }
             }
+            Collections.sort(resultList);
+            answerList.add("#"+num+" ");
+            answerList.addAll(resultList);
+            resultList.clear();
         }
-        for (int i = 0 ; i < resultList.size(); i++){
 
-            System.out.print(resultList.get(i));
+        for (int i = 0 ; i < answerList.size(); i++){
+            if (answerList.get(i).contains("#")) System.out.println();
+            System.out.print(answerList.get(i));
             System.out.print(" ");
         }
     }
