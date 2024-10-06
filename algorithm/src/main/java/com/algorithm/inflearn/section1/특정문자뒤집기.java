eefd279608de64f514ddeda011071b74d13f9d4c
sakang <rogersddt@naver.com>
2024-10-06 17:48:50 +0900

    2024-10-06 인프런 1.5 특정 문자 뒤집기

---
+++ algorithm/src/main/java/com/algorithm/inflearn/section1/특정문자뒤집기.java
@@ -0,0 +1,39 @@
+package com.algorithm.inflearn.section1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 특정 문자 뒤집기
+// 핵심:
+// Character.isAlphabetic -> 불리안 리턴 / 해당 문자가 알파벳이냐
+public class 특정문자뒤집기 {
+
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] charStr = br.readLine().toCharArray();
+
+        int lt = 0;
+        int rt = charStr.length-1;
+
+        while (lt < rt){
+            if (!Character.isAlphabetic(charStr[lt])){
+                lt ++;
+            }
+            else if (!Character.isAlphabetic(charStr[rt])){
+                rt--;
+            }
+            else{
+                char tmp = charStr[lt];
+                charStr[lt] = charStr[rt];
+                charStr[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+        String answer = String.valueOf(charStr);
+        System.out.println(answer);
+    }
+}
+
