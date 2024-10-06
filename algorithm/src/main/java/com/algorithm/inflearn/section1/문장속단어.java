2dff231e76c51ed657b469ddb4e4bb614dfc84fe
sakang <rogersddt@naver.com>
2024-10-06 16:47:25 +0900

    2024-10-06 인프런 1.3 문장 속 단어

---
+++ algorithm/src/main/java/com/algorithm/inflearn/section1/문장속단어.java
@@ -0,0 +1,28 @@
+package com.algorithm.inflearn.section1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 문장 내 가장 긴 단어 출력
+// 핵심:
+// 길이를 구해 단어 추출 => 단순 구현. 이전 길이를 담는 변수 생성할 것
+public class 문장속단어 {
+
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+
+        int cnt = 0;
+        String words = "";
+        for (String x : str){
+            int len = x.length();
+            if (len > cnt){
+                cnt = len;
+                words = x;
+            }
+        }
+        System.out.println(words);
+    }
+}
