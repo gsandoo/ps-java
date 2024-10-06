5973da3d0186c63903dcc806ab6a53807500580d
sakang <rogersddt@naver.com>
2024-10-06 17:12:55 +0900

    2024-10-06 인프런 1.4 단어 뒤집기

---
+++ algorithm/src/main/java/com/algorithm/inflearn/section1/단어뒤집기.java
@@ -0,0 +1,29 @@
+package com.algorithm.inflearn.section1;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+// 단어 뒤집기
+// 핵심:
+// StringBuilder 사용 -> 문자열 조합기
+public class 단어뒤집기 {
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb ;
+        List<String> list = new ArrayList<>();
+        for (int i = 0 ; i < N ; i++){
+           list.add(new StringBuilder(br.readLine()).reverse().toString());
+        }
+
+        for (int i = 0 ; i < list.size() ; i++){
+            System.out.println(list.get(i));
+        }
+    }
+
+}
+
