3113cc2bd35c71522a333420a1bb3a70b607a1f3
sakang <rogersddt@naver.com>
2024-12-13 01:03:00 +0900

    2024-12-12 boj solved.ac 브론즈 문제

---
+++ algorithm/src/main/java/com/algorithm/boj/ac/bronze/개.java
@@ -0,0 +1,11 @@
+package com.algorithm.boj.ac.bronze;
+
+public class 개 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+}
---
+++ algorithm/src/main/java/com/algorithm/boj/ac/bronze/나머지.java
@@ -0,0 +1,27 @@
+package com.algorithm.boj.ac.bronze;
+
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+
+/*
+* hash set :
+* 순서의 개념이 없다
+* 동일한 값 허용 안한다
+* 만약 정렬을 원하면 배열로 변환 후 collections.sort 해라
+*
+* */
+public class 나머지 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i = 0 ; i < 10 ; i++) {
+            hs.add(Integer.parseInt(br.readLine()) % 42);
+        }
+        System.out.println(hs.size());
+    }
+}
